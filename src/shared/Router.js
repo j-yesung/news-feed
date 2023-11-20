788b1adeca4456d460a3a2f73a7d6cd005aa2b0e
j-yesung <hisung-ah@kakao.com>
2023-11-20 19:51:24 +0900

    프로젝트 환경설정

---
+++ src/shared/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
