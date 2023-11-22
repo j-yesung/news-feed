cd88ff111ace6a6a20959777662be0a9aff659d9
byeongtaek <respect21047463@gmail.com>
2023-11-22 21:39:48 +0900

    닉네임가져오기구현

--- src/pages/Login/Signup.jsx
+++ src/pages/Login/Signup.jsx
@@ -1,5 +1,5 @@
-import React, { useState,useRef,useCallback } from 'react';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import React, { useState, useCallback } from 'react';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebase';
 
 const Signup = () => {
@@ -7,63 +7,72 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [twicePw, setTwicePw] = useState('');
-
-
-
+  console.log(nickname);
   // const ref =useRef("")
 
   // ref.current = "바꾼값"
 
-  const nameHandler = useCallback((e) => {
+  const nameHandler = useCallback(e => {
     setNickName(e.target.value);
-    console.log('nickname',e.target.value);
-  },[]);
-  const emailHandler = useCallback((e) => {
+  }, []);
+  const emailHandler = useCallback(e => {
     setEmail(e.target.value);
-    console.log('email',e.target.value);
-  },[]);
-  const passwordHandler = useCallback((e) => {
+  }, []);
+  const passwordHandler = useCallback(e => {
     setPassword(e.target.value);
-    console.log('password',e.target.value);
-  },[]);
-  const twicePwHandler = useCallback((e) => {
+  }, []);
+  const twicePwHandler = useCallback(e => {
     setTwicePw(e.target.value);
-    
-console.log('twicePw',e.target.value);
-  },[]);
+  }, []);
 
-  const signUp = useCallback(async (event) => {
+  const auth = getAuth();
+  updateProfile(auth.currentUser, {
+    displayName: nickname,
+    // photoURL: 'https://example.com/jane-q-user/profile.jpg',
+  });
+  const signUp = useCallback(event => {
     event.preventDefault();
-    try {
-      const { user } = await createUserWithEmailAndPassword(auth, email, password);
-      await updateProfile(auth.currentUser, { displayName: nickname });
-      return user;
-    } catch (error) {
-      // 에러 처리 (예: 에러 메시지 표시)
-      console.error('회원가입 중 에러 발생:', error);
-    }
-  }, [nickname, email, password]);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // 회원가입 성공시
+
+        // .then(() => {
+        //   // Profile updated!
+        //   // ...
+        // })
+        // .catch(error => {
+        //   // An error occurred
+        //   // ...
+        // });
+        console.log(userCredential);
+      })
+      .catch(error => {
+        // 회원가입 실패시
+        console.error(error);
+      });
+  }, []);
+
+  const user = auth.currentUser;
+  console.log('user', user);
 
   return (
     <div>
-      
-        <p>환영합니다</p>
-        <p>오늘의 나와 함께 갓생만들기 시작해요</p>
-        <div>
-          <input placeholder="닉네임" value={nickname} onChange={nameHandler} />
-          <br />
-          <input type='email' placeholder="이메일" value={email} onChange={emailHandler} />
-          <br />
-          <input placeholder="비밀번호" value={password} onChange={passwordHandler} />
-          <br />
-          <input placeholder="비밀번호재확인" value={twicePw} onChange={twicePwHandler} />
-        </div>
-        <div>
-          <button type="submit" onClick={signUp}>
-            회원가입
-          </button>
-        </div>
-   
+      <p>환영합니다</p>
+      <p>오늘의 나와 함께 갓생만들기 시작해요</p>
+      <div>
+        <input placeholder="닉네임" value={nickname} onChange={nameHandler} />
+        <br />
+        <input placeholder="이메일" value={email} onChange={emailHandler} />
+        <br />
+        <input placeholder="비밀번호" value={password} onChange={passwordHandler} />
+        <br />
+        <input placeholder="비밀번호재확인" value={twicePw} onChange={twicePwHandler} />
+      </div>
+      <div>
+        <button type="submit" onClick={signUp}>
+          회원가입
+        </button>
+      </div>
     </div>
   );
 };
