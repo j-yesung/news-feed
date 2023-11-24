56ace59318608b026dcacb8f45f5c1a619b0089a
j-yesung <hisung-ah@kakao.com>
2023-11-24 14:47:01 +0900

    [예성] App.jsx => 사용자 정보, 뉴스피드 불러오기 이사

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,34 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
 import { theme } from 'styles/theme';
 import Router from './shared/Router';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, newsFeedCollection } from './firebase';
+import { setUser } from 'redux/modules/user';
+import { getDocs } from 'firebase/firestore';
+import { setContents } from 'redux/modules/content';
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // 뉴스피드 불러오기
+    (async () => {
+      const querySnapshot = await getDocs(newsFeedCollection);
+      const data = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      dispatch(setContents(data));
+    })();
+    // 사용자 정보 불러오기
+    const subscribe = onAuthStateChanged(auth, user => {
+      dispatch(setUser(user));
+    });
+
+    return () => {
+      subscribe();
+    };
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
