6d318d94a933455f361b01dee6da24acc97658a4
allone9425 <allone9425@gmail.com>
2023-11-24 22:04:37 +0900

    [다원] 비밀번호 찾기 스타일링 완료

--- src/pages/Login/ModalBasic.jsx
+++ src/pages/Login/ModalBasic.jsx
@@ -4,23 +4,73 @@ import styled from 'styled-components';
 import { auth } from '../../firebase';
 
 const Bg = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
   position: absolute;
-  width: 100vw;
-  background-color: aliceblue;
+  h2 {
+    font-size: 24px;
+    font-weight: bold;
+  }
 `;
 
-const Modal = styled.div``;
+const Modal = styled.div`
+  background-color: #fff;
+  padding: 30px 20px;
+  width: 400px;
+  position: absolute;
+  left: 50%;
+  border-radius: 15px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 3px 3px 10px #555;
+`;
 
-const CloseBtn = styled.button``;
-const Email = styled.input``;
+const CloseBtn = styled.button`
+  font-size: 29px;
+  font-weight: bold;
+  border: none;
+  background-color: transparent;
+  transform: translate(0px, -2px);
+`;
+const Email = styled.input`
+  padding: 10px;
+  width: 100%;
+  outline: none;
+  border: 2px solid #ccc;
+  border-radius: 8px;
+  margin-bottom: 30px;
+  margin-top: 30px;
+  &::placeholder {
+    color: #ccc;
+  }
+`;
 
-export const ResetPwBtn = styled.button``;
+export const ResetPwBtn = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #f4eba5 !important;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+  margin-bottom: 10px;
+  &:hover {
+    background-color: #a5c7bb !important;
+  }
+`;
 
 const ModalBasic = ({ setModalOpen }) => {
   const [email, setEmail] = useState('');
   const closeModal = () => {
     setModalOpen(false);
   };
+
+  const NameClose = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `;
 
   const onChange = event => {
     setEmail(event.target.value);
@@ -39,8 +89,10 @@ const ModalBasic = ({ setModalOpen }) => {
     <>
       <Bg>
         <Modal>
-          <h2>비밀번호 재설정</h2>
-          <CloseBtn onClick={closeModal}>&#10005;</CloseBtn>
+          <NameClose>
+            <h2>비밀번호 재설정</h2>
+            <CloseBtn onClick={closeModal}>&#10005;</CloseBtn>
+          </NameClose>
           <form>
             <div>
               <Email type="email" value={email} placeholder="이메일" onChange={onChange}></Email>
