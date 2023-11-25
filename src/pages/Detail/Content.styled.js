e5a850cefe720d1a5acb2af71b9bdcc3c3d53655
allone9425 <allone9425@gmail.com>
2023-11-26 01:24:02 +0900

    [다원] 상세페이지 styling

--- src/pages/Detail/Content.styled.js
+++ src/pages/Detail/Content.styled.js
@@ -5,34 +5,45 @@ export const Avatar = styled.img`
 `;
 
 export const View = styled.div`
-  background-color: #fff;
+  background-color: skyblue;
   width: 70%;
   border-radius: 20px;
   padding: 8px 20px 20px;
   font-size: 20px;
   height: calc(100vh - 300px);
   position: relative;
-
-  button {
-    padding: 10px 20px;
-    border: none;
-    margin: 10px;
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-    font-size: 20px;
-    font-weight: bold;
-    border-radius: 5px;
-    background-color: #f4eba5;
-    &:first-of-type {
-      right: 100px;
-    }
-    &:hover {
-      background-color: #a5c7bb;
-    }
-  }
 `;
 
+export const ModifyBtn = styled.button`
+  padding: 10px 20px;
+  border: none;
+  margin: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  position: absolute;
+  cursor: pointer;
+  right: 110px;
+  border-radius: 5px;
+  background-color: #f4eba5;
+  &:hover {
+    background-color: #a5c7bb;
+  }
+`;
+export const DeleteBtn = styled.button`
+  padding: 10px 20px;
+  border: none;
+  margin: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  background-color: #f4eba5;
+  &:hover {
+    background-color: #a5c7bb;
+  }
+`;
 export const Box = styled.div`
   display: flex;
   justify-content: center;
@@ -43,9 +54,9 @@ export const Box = styled.div`
 `;
 
 export const Name = styled.p`
-  //background-color: azure;
-  padding: 10px;
-  margin-left: 10px;
+  // background-color: azure;
+  padding: 10px 5px;
+  margin-left: 20px;
   font-weight: bold;
   width: 300px;
   font-size: 23px;
@@ -55,17 +66,17 @@ export const AvatarName = styled.div`
   display: flex;
   //background-color: lemonchiffon;
   align-items: center;
+  width: 100%;
+
   padding: 5px 0 10px;
   border-bottom: 2px solid #ccc;
 `;
 
 export const Date = styled.p`
   //background-color: antiquewhite;
-  position: absolute;
-  padding: 10px;
-  font-weight: bold;
-  right: 20px;
-  top: 17px;
+  margin-left: 20px;
+  padding: 5px;
+  font-size: 16px;
 
   @media (max-width: 1099px) {
     //
@@ -75,13 +86,18 @@ export const Date = styled.p`
 export const Content = styled.div`
   line-height: 2rem;
   height: 370px;
+  background-color: lemonchiffon;
   overflow: auto;
+  width: 700px;
+  padding-right: 15px;
 `;
 
 export const Title = styled.h2`
   font-weight: bold;
   font-size: 28px;
   padding: 30px 0 20px;
+  background-color: aliceblue;
+  width: 700px;
 `;
 
 export const Textarea = styled.textarea`
@@ -106,4 +122,9 @@ export const InputTitle = styled.input`
   margin: 20px 0 10px;
   outline: none;
   border-radius: 10px;
+`;
+
+export const ViewCommentBox = styled.div`
+  //background-color: blueviolet;
+  display: flex;
 `;
