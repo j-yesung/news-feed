b6719291b38ab8cfe3ec37896275009b9b820877
allone9425 <allone9425@gmail.com>
2023-11-26 22:12:12 +0900

    [다원] 마이페이지 PC버전 완성

--- src/pages/User/Profile.jsx
+++ src/pages/User/Profile.jsx
@@ -22,7 +22,6 @@ const Profile = () => {
 
   return authUser ? (
     <>
-      <div>마이페이지</div>
       {/* FileUpload : 파일 업로드 컴포넌트 */}
       <S.ProfileBox>
         <FileUpload />
@@ -51,23 +50,17 @@ const Profile = () => {
           )}
         </S.ProfileTop>
       </S.ProfileBox>
-      {authUser &&
-        myContents.map(contents => (
-          <div
-            key={contents.id}
-            onClick={() => navigate(`/content/${contents.id}`)}
-            style={{
-              border: '1px solid #292929',
-              padding: '10px',
-              margin: '10px',
-              cursor: 'pointer',
-            }}>
-            <div>날짜 : {contents.date}</div>
-            <div>제목 : {contents.title}</div>
-            <div>내용 : {contents.content}</div>
-            <br />
-          </div>
-        ))}
+      <S.BulletinBox>
+        <S.MyBulletin>{authUser.displayName}의 게시글</S.MyBulletin>
+        {authUser &&
+          myContents.map(contents => (
+            <S.Bulletin key={contents.id} onClick={() => navigate(`/content/${contents.id}`)}>
+              <S.Title>제목 : {contents.title}</S.Title>
+              <S.Contents>{contents.content}</S.Contents>
+              <S.Date>날짜 : {contents.date}</S.Date>
+            </S.Bulletin>
+          ))}
+      </S.BulletinBox>
     </>
   ) : null;
 };
--- src/pages/User/Profile.styled.js
+++ src/pages/User/Profile.styled.js
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
 export const ProfileTop = styled.div`
-  background-color: lemonchiffon;
+  //background-color: lemonchiffon;
   width: 100%;
-
   margin-left: 20px;
-  line-height: 3rem;
+  line-height: 2rem;
 `;
 export const ProfileName = styled.div`
-  background-color: blanchedalmond;
+  // background-color: blanchedalmond;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
 export const ChangeName = styled.button`
   background-color: #f4eba5;
   border: none;
   font-weight: bold;
+  width: 500px;
   padding: 10px 20px;
   cursor: pointer;
+  width: 400px;
   border-radius: 5px;
   &:hover {
     background-color: #a5c7bb;
@@ -31,31 +33,89 @@ export const SaveBtn = styled.button`
   font-weight: bold;
   padding: 10px 20px;
   border-radius: 5px;
+  width: 400px;
+
   cursor: pointer;
   &:hover {
     background-color: #a5c7bb;
   }
 `;
 export const ProfileBox = styled.div`
-  background-color: aquamarine;
+  background-color: #fff;
+  border-radius: 10px;
   display: flex;
+  align-items: center;
+  padding: 10px;
   width: 900px;
-  margin: 0 auto;
+  margin: 130px auto 30px;
+  box-shadow: 3px 3px 10px #aaa;
 `;
 
 export const MyEmail = styled.p`
   font-size: 1.3rem;
+  text-align: center;
+  padding-left: 10px;
 `;
 
 export const NowNickName = styled.p`
   font-size: 1.3rem;
   padding-left: 10px;
+
+  width: 500px;
+  margin: 20px 0;
+  text-align: center;
 `;
 
 export const ChangeInput = styled.input`
   outline: none;
   border: none;
-  width: 570px;
+  width: 350px;
+  text-align: center;
   padding: 10px;
+  border-radius: 10px;
+  margin: 20px 0;
+  border: 1px solid #ccc;
   font-size: 1.3rem;
+`;
+
+export const MyBulletin = styled.h2`
+  font-weight: bold;
+  font-size: 1.5rem;
+  border-bottom: 5px solid #a5c7bb;
+  padding-bottom: 5px;
+`;
+
+export const Bulletin = styled.div`
+  background-color: #fff;
+  padding: 10px;
+  cursor: pointer;
+  margin: 30px 0;
+  border-radius: 10px;
+  box-shadow: 3px 3px 10px #aaa;
+  padding: 20px;
+`;
+
+export const BulletinBox = styled.div`
+  width: 900px;
+  margin: 50px auto 20px;
+  font-size: 1.2rem;
+
+  line-height: 2rem;
+`;
+
+export const Title = styled.h2`
+  font-weight: bold;
+  font-size: 1.4rem;
+  padding-bottom: 10px;
+  border-bottom: 2px solid #a5c7bb;
+`;
+
+export const Date = styled.p`
+  border-top: 2px solid #a5c7bb;
+  padding-top: 14px;
+  text-align: right;
+`;
+
+export const Contents = styled.div`
+  padding: 20px 0;
 `;
