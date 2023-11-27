1e8e46c8a2f158ca033a782c2e5a3ef72fb116f4
j-yesung <hisung-ah@kakao.com>
2023-11-27 10:54:37 +0900

    [예성] Nav active 추가

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { UseSelector } from 'react-redux/es/hooks/useSelector';
-import { Dispatch } from 'react';
+import { Dispatch, useState } from 'react';
 import { setcategory } from 'redux/modules/navbar';
 const NavContainer = styled.nav`
   width: 200px;
@@ -15,45 +15,61 @@ const NavContainer = styled.nav`
     display: ${props => (props.$isVisible ? 'block' : 'none')};
   }
 `;
-
 const Menu = styled.ul`
   li {
     margin: 10px 0;
   }
 `;
-
 const MenuBox = styled.ul`
   margin: 20px;
   padding: 3px 10px;
   background-color: #fff;
   border-radius: 10px;
-
-  li {
-    padding: 10px 0;
-    margin: 10px 0;
-    width: 130%;
-    border-radius: 10px;
-    position: relative;
-    left: 50%;
-    transform: translate(-50%, 0px);
-    &:hover {
-      background-color: ${({ theme }) => theme.contentsHeaderColor};
-      cursor: pointer;
-      font-weight: bold;
-      color: ${({ theme }) => theme.buttonTextColor};
-    }
-  }
+  color: ${({ theme }) => theme.contentsTextColor};
   a {
     text-decoration: none;
     color: ${({ theme }) => theme.contentsTextColor};
   }
 `;
 
+const MenuItem = styled.li`
+  padding: 10px 0;
+  margin: 10px 0;
+  width: 130%;
+  border-radius: 10px;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 0px);
+  &:hover {
+    background-color: ${({ theme }) => theme.contentsHeaderColor};
+    color: ${({ theme }) => theme.buttonTextColor};
+    cursor: pointer;
+    font-weight: bold;
+  }
+  ${({ $active, theme }) =>
+    $active &&
+    `
+    background-color: ${theme.contentsHeaderColor};
+    font-weight: bold;
+    color: ${theme.buttonTextColor};
+  `};
+`;
+
 const Nav = ({ isVisible }) => {
   const navigator = useNavigate();
   const authUser = useSelector(state => state.user.user);
-
   const dispatch = useDispatch();
+  const [isActive, setIsActive] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
+
+  const profilePage = () => {
+    !authUser ? alert('로그인이 필요합니다.') : navigator('/profile');
+  };
+  const handleMenuItemClick = index => {
+    setActiveMenuItem(index);
+    setIsActive(!isActive);
+  };
+
   const MenuBarFiltering = category => {
     dispatch(setcategory(category));
   };
@@ -63,39 +79,68 @@ const Nav = ({ isVisible }) => {
       <Menu>
         <MenuBox>
           <Link to="/">
-            <li onClick={() => MenuBarFiltering('default')}>메인으로</li>
+            <MenuItem
+              $active={activeMenuItem === 0}
+              onClick={() => {
+                handleMenuItemClick(0);
+                MenuBarFiltering('default');
+              }}>
+              메인으로
+            </MenuItem>
           </Link>
         </MenuBox>
         <li>
-          {/* 첫번째 섹션 */}
           <MenuBox>
-            <li onClick={() => MenuBarFiltering('대학생')}>대학생</li>
-            <li onClick={() => MenuBarFiltering('직장인')}>직장인</li>
-            <li onClick={() => MenuBarFiltering('음악')}>음악</li>
-            <li onClick={() => MenuBarFiltering('스포츠')}>스포츠</li>
+            <Link to="/">
+              <MenuItem
+                $active={activeMenuItem === 2}
+                onClick={() => {
+                  handleMenuItemClick(2);
+                  MenuBarFiltering('대학생');
+                }}>
+                대학생
+              </MenuItem>
+              <MenuItem
+                $active={activeMenuItem === 3}
+                onClick={() => {
+                  handleMenuItemClick(3);
+                  MenuBarFiltering('직장인');
+                }}>
+                직장인
+              </MenuItem>
+              <MenuItem
+                $active={activeMenuItem === 4}
+                onClick={() => {
+                  handleMenuItemClick(4);
+                  MenuBarFiltering('음악');
+                }}>
+                음악
+              </MenuItem>
+              <MenuItem
+                $active={activeMenuItem === 5}
+                onClick={() => {
+                  handleMenuItemClick(5);
+                  MenuBarFiltering('스포츠');
+                }}>
+                스포츠
+              </MenuItem>
+            </Link>
           </MenuBox>
         </li>
         <li>
-          {/* 두번째 섹션 */}
           <MenuBox>
-            <li>5번째 섹션</li>
-            <li>6번째 섹션</li>
-            <li>7번째 섹션</li>
-            <li>8번째 섹션</li>
-            <li>9번째 섹션</li>
-            <li>10번째 섹션</li>
-          </MenuBox>
-        </li>
-        <li>
-          {/* 세번째 섹션 */}
-          <MenuBox>
-            <li onClick={() => (!authUser ? alert('로그인이 필요합니다.') : navigator('/profile'))}>마이 페이지</li>
-            <li>로그아웃</li>
+            <MenuItem
+              $active={activeMenuItem === 1}
+              onClick={() => {
+                profilePage();
+                handleMenuItemClick(1);
+              }}>
+              마이 페이지
+            </MenuItem>
           </MenuBox>
         </li>
       </Menu>
     </NavContainer>
   );
 };
-
 export default Nav;
